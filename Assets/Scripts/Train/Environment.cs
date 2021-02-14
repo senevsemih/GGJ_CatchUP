61d29441b61d159dc75d2b8038b34e35f5388b82
senevsemih <senevsemih@gmail.com>
2021-02-14 18:24:00 +0300

    train script

---
+++ Assets/Scripts/Train/Environment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    [SerializeField] private Transform bar;
+
+    private float speed = 20f;
+
+    void Update()
+    {
+        if (bar.localRotation.z > 0.7)
+        {
+            speed = 0;
+            StartCoroutine(TheyLost());
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Papa"))
+        {
+            InvokeRepeating("MoveBarDown", 0f, .01f);
+        }
+    }
+
+    void MoveBarDown()
+    {
+        bar.Rotate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    IEnumerator TheyLost()
+    {
+        yield return new WaitForSeconds(8f);
+        
+        LevelChanger.Instance.LoadNextLevel();
+    }
+}
---
+++ Assets/Scripts/Train/Train.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Train : MonoBehaviour
+{
+    private const float MoveUnitPerSeconds = 3f;
+
+    void Update()
+    {
+        this.Wait(.5f, Move);
+    }
+
+    void Move()
+    {
+        Vector3 position = transform.position;
+        position.x -= MoveUnitPerSeconds * Time.deltaTime;
+        transform.position = position;
+    }
+}
