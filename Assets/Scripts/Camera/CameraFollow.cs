931b0a8cb0a837bffadfe010b2f34151ba165c8b
senevsemih <senevsemih@gmail.com>
2021-02-14 18:25:14 +0300

    camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject jimmy;
+
+    private Vector3 offset;
+
+    void Awake()
+    {
+        jimmy = GameObject.FindGameObjectWithTag("Jason");
+    }
+    
+    void Start()
+    {
+        offset = transform.position - jimmy.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = jimmy.transform.position + offset;
+    }
+}
