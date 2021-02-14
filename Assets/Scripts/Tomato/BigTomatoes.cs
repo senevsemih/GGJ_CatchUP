1a1ac5ae2ec5cf5da35c64072d549332ea4c251b
senevsemih <senevsemih@gmail.com>
2021-02-14 18:25:29 +0300

    tomatoes controller

---
+++ Assets/Scripts/Tomato/BigTomatoes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BigTomatoes : MonoBehaviour
+{
+    private const float MoveUnitPerSeconds = 5f;
+
+    private AudioClip[] clips = new AudioClip[2];
+
+    void Start()
+    {
+        InvokeRepeating("Voice", 2f, 5f);
+    }
+    
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.z += MoveUnitPerSeconds * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void Voice()
+    {
+        AudioController.Instance.Play(AudioController.Instance.catchup);
+        
+        this.Wait(1.5f, () =>
+        {
+            AudioController.Instance.Play(AudioController.Instance.jason);
+        });
+    }
+}
---
+++ Assets/Scripts/Tomato/LittleTomato.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LittleTomato : MonoBehaviour
+{
+    private float moveUnitPerSeconds = 6f;
+    private Rigidbody rb;
+    
+    private GameObject trainPrefab;
+    [SerializeField] private Transform trainPosition;
+    
+    bool isTrainSpawned = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        trainPrefab = (GameObject)Resources.Load("Prefabs/Environment/Train");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(direction * moveUnitPerSeconds * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bar") && !isTrainSpawned)
+        {
+            GameObject train = Instantiate(trainPrefab);
+            train.transform.position = trainPosition.position;
+
+            isTrainSpawned = true;
+        }
+        
+        if (other.gameObject.CompareTag("Foot"))
+        {
+            
+            LevelChanger.Instance.LoadLevel();
+        }
+
+        if (other.gameObject.CompareTag("Portal"))
+        {   
+            LevelChanger.Instance.LoadNextLevel();
+        }
+    }
+}
